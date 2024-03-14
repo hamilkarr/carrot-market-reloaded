b1d96673375efcfe0929a0fca4d5ed911fdbd82b
hamilkarr <12uclock@gmail.com>
2024-03-15 00:50:19 +0900

    스타일 및 레이아웃 변경사항 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
-        <main className="h-screen flex items-center justify-center p-5">
-            <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-                <a href="#">aaa</a>
-                <button className="w-full bg-black h-10 text-white rounded-sexy-name ">Submit</button>
+        <div className="flex flex-col items-center justify-between min-h-screen p-5">
+            <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+                <span className="text-9xl">🥕</span>
+                <h1 className="text-4xl">당근</h1>
+                <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
             </div>
-        </main>
+            <div className="flex flex-col items-center gap-3 w-full">
+                <Link
+                    href="/create-account"
+                    className="primary-btn py-2.5 text-lg"
+                >
+                    시작하기
+                </Link>
+                <div className="flex gap-2">
+                    <span>이미 계정이 있나요?</span>
+                    <Link href="/login" className="hover:underline">
+                        로그인
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 }
