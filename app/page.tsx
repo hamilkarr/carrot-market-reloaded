d6e6b9be8eb49a3ae360728a628db9cd4e17a50a
hamilkarr <12uclock@gmail.com>
2024-03-15 00:08:35 +0900

    tailwind 수업 완료

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ export default function Home() {
     return (
         <main className="h-screen flex items-center justify-center p-5">
             <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-                <div className="group flex flex-col">
-                    <input type="text" className="bg-gray-100 w-full" placeholder="Write your email" />
-                    <span className="group-focus-within:block hidden">Make sure it is a valid email...</span>
-                    <button>Submit</button>
-                </div>
+                <a href="#">aaa</a>
+                <button className="w-full bg-black h-10 text-white rounded-sexy-name ">Submit</button>
             </div>
         </main>
     );
