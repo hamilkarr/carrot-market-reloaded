311874f88b608b534eed2d0c9affa37e417de0d5
hamilkarr <12uclock@gmail.com>
2024-03-16 08:13:19 +0900

    로그인 페이지와 관련된 코드 변경 사항을 커밋합니다.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,26 +3,33 @@ import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 
 export default function LogIn() {
+    async function handleForm(data: FormData) {
+        'use server';
+        console.log('I run in the server only!');
+        console.log(data.get("email"), data.get("password"));
+    }
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form className="flex flex-col gap-3">
+            <form action={handleForm} className="flex flex-col gap-3">
                 <FormInput
+                    name="email"
                     type="email"
                     placeholder="Email"
-                    required
+                    required={true}
                     errors={[]}
                 />
                 <FormInput
+                    name="password"
                     type="password"
                     placeholder="Password"
-                    required
+                    required={true}
                     errors={[]}
                 />
-                <FormButton loading={false} text="Create account" />
+                <FormButton loading={false} text="Log in" />
             </form>
             <SocialLogin />
         </div>
